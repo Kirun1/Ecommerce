a8cd99364f5282a4f4175ec8687a1fd3395c43e4
Sreelal <sreelal.ub@gmail.com>
2018-04-21 12:42:23 +0530

    index page changes

--- application/controllers/dashboard/Welcome.php
+++ application/controllers/dashboard/Welcome.php
@@ -4,8 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	public function index()
-	{
+	{	
 		$this->load->view('dashboard/header');
+		$this->load->view('dashboard/index');
+		$this->load->view('dashboard/footer');
 	}
 
 }
